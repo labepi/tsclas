26377cdf7215983b17704335c4c360aad318cea4
Joao Borges <joaoborges@dct.ufrn.br>
2020-07-20 23:48:36 -0300

    Adding script to dowload data for 3 months from ASOS 1hour

---
+++ data/asos/1hour/crawler_iem_1hour.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# script to get 1min data for 1 month for the stations listed in
+# stationsfile
+
+# 00,01,...,05
+stations_file="airports"$1".csv"
+
+save_dir='./raw'
+
+url1="https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?station="
+url2="&data=all&year1=2020&month1=1&day1=1&year2=2020&month2=3&day2=31&tz=Etc%2FUTC&format=onlycomma&latlon=yes&missing=M&trace=T&direct=no&report_type=1&report_type=2"
+
+for i in $(cat $stations_file | cut -d',' -f2 | grep -v ident | sed 's/"//g')
+do
+    echo $i
+    url=$url1$i$url2
+
+    #echo $url
+
+    wget -4 $url -O $save_dir/$i.csv -o wget.log.out
+   
+    # the caller will control this
+    #2> /dev/null
+
+    #exit
+done
+
