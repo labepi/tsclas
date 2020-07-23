b79061ba84cd791ce89dfb16db3acc189263bbbc
Joao Borges <joaoborges@dct.ufrn.br>
2020-07-23 12:37:05 -0300

    Adding script to compute features for asos 1hour

--- computing_features_asos.sh
+++ computing_features_asos.sh
@@ -10,13 +10,17 @@ fi
 
 num_tau=30
 
+# time span is informed by command line
+time_span=$2
+
 # name
-d_name='asos_2020_jan_1month'
+d_name="asos_2020_jan_"$time_span"_1hour.csv"
 
 # path
-dataset_path='./data/asos/1min'
+dataset_path='./data/asos/1hour'
 
-Rscript computing_features.R $d_name $dataset_path $D $num_tau
+Rscript computing_features.R $d_name $dataset_path $D $num_tau \ 
+        &> "outputs/output_"$d_name"_D"$D".out" 
 
 # getting the fields names
 #for d_name in $(cat data/botnet/demonstrate_structure.csv | sed 's/,/ /g')
