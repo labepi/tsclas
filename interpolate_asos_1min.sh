165d2636f64cbfb5b9cd1c351b8b0d5b54311d92
Joao Borges <joaoborges@dct.ufrn.br>
2020-07-31 11:51:20 -0300

    Adding time_span as an argument for interpolation

--- interpolate_asos_1min.sh
+++ interpolate_asos_1min.sh
@@ -7,19 +7,20 @@ dataset_path='./data/asos/1min'
 time_int_l=( "1min" "5min" "10min" "15min" )
 
 # time span
-time_span_l=( "1day" "1week" "2week" "3week" "1month" )
+#time_span_l=( "1day" "1week" "2week" "3week" "1month" )
 
-for time_span in "${time_span_l[@]}"
-do 
-    for time_int in "${time_int_l[@]}"
-    do
-        d_name=$dataset_path"/asos_2020_jan_"$time_span"_"$time_int".csv"
-        
-        echo $d_name
-        #continue
+# NOTE: making timespan as an argument, to run separated processes
+time_span=$1
 
-        python3 interpolate.py $d_name
-        
-    done
+#for time_span in "${time_span_l[@]}"
+for time_int in "${time_int_l[@]}"
+do
+    d_name=$dataset_path"/asos_2020_jan_"$time_span"_"$time_int".csv"
+    
+    echo $d_name
+    #continue
+
+    python3 interpolate.py $d_name
+    
 done
 
