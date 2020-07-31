fd5b30b10666a4bdb1825ca8632f981997ed7099
Joao Borges <joaoborges@dct.ufrn.br>
2020-07-31 13:59:34 -0300

    Adding classification scripts for the sktime/literature

--- classify_sktime.py
+++ classify_sktime.py
@@ -4,6 +4,7 @@
 import numpy as np
 import pandas as pd
 
+import sys
 import time
 
 # classifiers
@@ -35,6 +36,15 @@ from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import VotingClassifier
 from sklearn.model_selection import GridSearchCV, LeaveOneOut
 
+
+if (len(sys.argv) != 4):
+    quit('Error on arguments')
+
+# getting arguments
+d_path=sys.argv[1]
+d_name=sys.argv[2]
+seed=int(sys.argv[3])
+
 # used only for the sktime algorithms
 def from_table_to_data(x):
     instance_list = []
@@ -52,119 +62,97 @@ def from_table_to_data(x):
     return x_data
 
 # number of runs
-#numtotal = 10
-numtotal = 1
-startseq = 1
+#numtotal = 30
+#numtotal = 1
+#startseq = 1
 
-# looping for each time series cut
-#for i in range(25):
-#for i in range(26):
-for i in range(1):
-#for i in range(16,26):
-#for i in range(15,25): # not month
-    #if i == 23:
-    #    name = '1day'
-    #elif i == 24:
-    #    name = '1week'
-    #elif i == 25:
-    #    name = '1month'
-    #else:
-    #    name = str(i+1)+'hour'
-    #    #name = str(i+1+10)+'hour'
-    
-    # RAW time series
-    
-    # jan 2020
-    #d_name = 'data/asos/1min/asos_2020_jan_1day_15min.csv' 
-    d_name = 'data/asos/1min/asos_2020_jan_1day_15min_spline.csv' 
-    
-    # fev/2020
-    #d_name = 'data/datasets/1min_2020_fev_raw/asos_1min_2020_fev_'+name+'_spline.csv' 
-    #d_name = '../../../phenomena/sources/wunder_2015jan_spline.csv' 
+# RAW time series
 
-    # FEATS
-    #d_name = 'data/datasets/1min_2020_fev_feats/D3/asos_1min_2020_fev_'+name+'_spline_feats.csv' # fev/2020
-    #d_name = 'data/datasets/1min_2020_fev_feats/D4/asos_1min_2020_fev_2hour_spline_feats.csv' # fev/2020
-    #d_name = 'data/datasets/1min_2020_fev_feats/D5/asos_1min_2020_fev_2hour_spline_feats.csv' # fev/2020
-    #d_name = 'data/datasets/1min_2020_fev_feats/D6/asos_1min_2020_fev_2hour_spline_feats.csv' # fev/2020
-    
-    # loading data
-    X = pd.read_csv(d_name, header=None)
-    #print('loaded')
-    
-    y = X.iloc[:,-1] # 8736
-    X = X.iloc[:,:-1] # all columns except last one
+# jan 2020
+#d_name = 'data/asos/1min/asos_2020_jan_1day_15min.csv' 
+#d_name = 'data/asos/1min/asos_2020_jan_1day_15min_spline.csv' 
 
-    print(X.shape)
-    
-    # looping for each simulation
-    for rstate in range(startseq,startseq+numtotal):
-        
-        print(d_name, end=' ', flush=True)
+# loading data
+X = pd.read_csv(d_path+'/'+d_name+'.csv', header=None)
 
-        # spliting train/test
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, 
-                random_state=rstate)
-        
-        # converting to sktime format
-        X_train2 = from_table_to_data(X_train)
-        X_test2 = from_table_to_data(X_test)
-        y_train2 = np.asarray(y_train)
-        y_test2 = np.asarray(y_test)
+# removing the label column
+y = X.iloc[:,-1] # last column
+X = X.iloc[:,:-1] # all columns except last one
 
-        ### TODO: here is the classification step
-        ###   - check the algorithms in scikit learn
-        ###   - check the cross-validation options
-        ###   - do the ensemble here
-        
-        t = time.process_time()
-        knn = KNeighborsTimeSeriesClassifier(metric="dtw")
-        knn.fit(X_train2, y_train2) # ignore the FutureWarning from sklearn
-        y_pred = knn.predict(X_test2)
-        acc = accuracy_score(y_test, y_pred)
-        lap = time.process_time() - t
-        print('knn: '+str(acc)+' '+str(lap), end=' ', flush=True)
+#print(X.shape)
 
-        t = time.process_time()
-        randf = RandomForestClassifier()
-        randf.fit(X_train, y_train)
-        y_pred = randf.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
-        lap = time.process_time() - t
-        print('randf: '+str(acc)+' '+str(lap), end=' ', flush=True)
-        
-        t = time.process_time()
-        tsf = TimeSeriesForestClassifier()
-        tsf.fit(X_train2, y_train2)
-        acc = tsf.score(X_test2, y_test2)
-        lap = time.process_time() - t
-        print('tsf: '+str(acc)+' '+str(lap), end=' ', flush=True)
+# looping for each simulation
+#for seed in range(startseq,startseq+numtotal):
 
-        t = time.process_time()
-        rise = RandomIntervalSpectralForest()
-        rise.fit(X_train2, y_train2)
-        acc = rise.score(X_test2, y_test2)
-        lap = time.process_time() - t
-        print('rise: '+str(acc)+' '+str(lap), end=' ', flush=True)
-        
-        #t = time.process_time()
-        #st = ShapeletTransformClassifier()
-        #st.fit(X_train2, y_train2)
-        #acc = st.score(X_test2, y_test2)
-        #lap = time.process_time() - t
-        #print('st: '+str(acc)+' '+str(lap), end=' ', flush=True)
-        
-        # TODO: check if remove boss for less i's
-        if i >= 15:
-        #if i >= 0:
-            print('boss: 0 0', flush=True)
-        else:
-            t = time.process_time()
-            boss = BOSSEnsemble()
-            boss.fit(X_train2, y_train2)
-            y_pred = boss.predict(X_test2)
-            acc = accuracy_score(y_test, y_pred)
-            lap = time.process_time() - t
-            print('boss: '+str(acc)+' '+str(lap), flush=True)
+print(d_name, end=' ', flush=True)
 
+# spliting train/test
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+        test_size=0.2, random_state=seed)
+
+# converting to sktime format
+X_train2 = from_table_to_data(X_train)
+X_test2 = from_table_to_data(X_test)
+y_train2 = np.asarray(y_train)
+y_test2 = np.asarray(y_test)
+
+# classifiers
+
+# knn
+t = time.process_time()
+knn = KNeighborsTimeSeriesClassifier(metric="dtw")
+knn.fit(X_train2, y_train2) # ignore the FutureWarning from sklearn
+y_pred = knn.predict(X_test2)
+acc = accuracy_score(y_test, y_pred)
+lap = time.process_time() - t
+print('knn: '+str(acc)+' '+str(lap), end=' ', flush=True)
+
+# randf
+t = time.process_time()
+randf = RandomForestClassifier(random_state=seed)
+randf.fit(X_train, y_train)
+y_pred = randf.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+lap = time.process_time() - t
+print('randf: '+str(acc)+' '+str(lap), end=' ', flush=True)
+
+# tsf
+t = time.process_time()
+tsf = TimeSeriesForestClassifier(random_state=seed)
+tsf.fit(X_train2, y_train2)
+y_pred = tsf.predict(X_test2)
+acc = accuracy_score(y_test, y_pred)
+lap = time.process_time() - t
+print('tsf: '+str(acc)+' '+str(lap), end=' ', flush=True)
+
+# rise
+t = time.process_time()
+rise = RandomIntervalSpectralForest(random_state=seed)
+rise.fit(X_train2, y_train2)
+y_pred = rise.predict(X_test2)
+acc = accuracy_score(y_test, y_pred)
+lap = time.process_time() - t
+print('rise: '+str(acc)+' '+str(lap), end=' ', flush=True)
+
+#t = time.process_time()
+#st = ShapeletTransformClassifier()
+#st.fit(X_train2, y_train2)
+#acc = st.score(X_test2, y_test2)
+#lap = time.process_time() - t
+#print('st: '+str(acc)+' '+str(lap), end=' ', flush=True)
+
+# TODO: check if remove boss for less i's
+#if i >= 15:
+##if i >= 0:
+#    print('boss: 0 0', flush=True)
+#else:
+#    t = time.process_time()
+#    boss = BOSSEnsemble()
+#    boss.fit(X_train2, y_train2)
+#    y_pred = boss.predict(X_test2)
+#    acc = accuracy_score(y_test, y_pred)
+#    lap = time.process_time() - t
+#    print('boss: '+str(acc)+' '+str(lap), flush=True)
+
+print(' ', flush=True)
 
---
+++ classify_sktime_asos_1min.sh
@@ -0,0 +1,28 @@
+
+# script for automatizing the classification call
+
+total=30 # also for the seed
+seq_begin=1
+
+dataset_path='./data/asos/1min'
+
+# time interval 
+time_int_l=( "1min" "5min" "10min" "15min" )
+
+# NOTE: making timespan as an argument, to run separated processes
+time_span=$1
+
+for time_int in "${time_int_l[@]}"
+do 
+    for seed in $(seq $seq_begin $((seq_begin + total - 1)))
+    do
+        d_name="asos_2020_jan_"$time_span"_"$time_int"_spline"
+        
+        #echo $d_name $seed
+        #continue
+
+        python3 classify_sktime.py $dataset_path $d_name $seed 
+        
+    done
+done
+
