62669fef505a116baa7eaf32b14560e9996b51f7
Joao Borges <joaoborges@dct.ufrn.br>
2020-08-11 02:49:54 -0300

    adding interpolation method

--- interpolate.py
+++ interpolate.py
@@ -9,14 +9,17 @@ import time
 #d_name = 'data/asos/1min/asos_2020_jan_1day_15min.csv' # jan/2020
 #dsname = 'data/asos/1min/asos_2020_jan_1day_15min_spline.csv' # jan/2020
 
-method='linear'
-#method='polynomial'
-#method='spline'
 
 # the path and name of the file to interpolate
 d_name = sys.argv[1]
 dsname = d_name.replace(".csv", "_"+method+".csv")
 #dsname = d_name.replace(".csv", "_spline.csv")
+
+# getting the interpolation method as argument
+method = sys.argv[2]
+#method='linear'
+#method='polynomial'
+#method='spline'
 
 #print(d_name)
 #print(dsname)
@@ -38,14 +41,15 @@ startTotal = time.time()
 ## filling NA values
 #X.interpolate(method='linear', axis=1, inplace=True,limit_direction='both')
 #X.interpolate(method='polynomial', order=3,axis=1,inplace=True,limit_direction='both')
-X.interpolate(method='spline', order=3, axis=1, inplace=True,limit_direction='both')
+#X.interpolate(method='spline', order=3, axis=1, inplace=True,limit_direction='both')
+X.interpolate(method=method, order=3, axis=1, inplace=True,limit_direction='both')
 X.fillna(0, inplace=True) # for filling the sides
 
 # end time
 endTotal = time.time()
 
 # total time taken
-print("TOTAL TIME:", endTotal - startTotal)
+print("TOTAL TIME:", method,' ',endTotal - startTotal)
 
 # returning names and y columns
 X.insert(0, 'names', names)
