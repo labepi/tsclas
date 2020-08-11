0ecc82fafe395551846be32461113d08493e7d11
Joao Borges <joaoborges@dct.ufrn.br>
2020-08-11 02:35:21 -0300

    ading support for the interpolation method

--- classify_sktime_asos_1min.sh
+++ classify_sktime_asos_1min.sh
@@ -12,11 +12,14 @@ time_int_l=( "1min" "5min" "10min" "15min" )
 # NOTE: making timespan as an argument, to run separated processes
 time_span=$1
 
+# spline/linear/polynomial
+method=$2
+
 for time_int in "${time_int_l[@]}"
 do 
     for seed in $(seq $seq_begin $((seq_begin + total - 1)))
     do
-        d_name="asos_2020_jan_"$time_span"_"$time_int"_spline"
+        d_name="asos_2020_jan_"$time_span"_"$time_int"_"$method
         
         #echo $d_name $seed
         #continue
