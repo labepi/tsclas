c0fca0d0bad1d28e66b74c785d7714cac7bfda6e
Joao Borges <joaoborges@dct.ufrn.br>
2020-08-27 10:52:53 -0300

    interpolation method as parameter

--- classify_asos_1min_all_interpolation.sh
+++ classify_asos_1min_all_interpolation.sh
@@ -21,7 +21,7 @@ time_int_l=( "1min" "5min" "10min" "15min" )
 # NOTE: making timespan as an argument, to run separated processes
 time_span=$2
 
-method='linear'
+method=$3
 
 for time_int in "${time_int_l[@]}"
 do 
