f4a83c0e8844fbdf79ad39678ab42d91775e8a90
Joao Borges <joaoborges@dct.ufrn.br>
2020-09-03 00:15:18 -0300

    separating algorithms and counting train/test times separately

--- classify_sktime.py
+++ classify_sktime.py
@@ -38,6 +38,18 @@ from sklearn.model_selection import GridSearchCV, LeaveOneOut
 
 #import impyute as impy
 
+# - knn
+# https://sktime.org/modules/auto_generated/sktime.classification.distance_based.KNeighborsTimeSeriesClassifier.html
+# - randf
+# https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+# - tsf
+# https://sktime.org/modules/auto_generated/sktime.classification.interval_based.TimeSeriesForest.html
+# - rise
+# https://sktime.org/modules/auto_generated/sktime.classification.frequency_based.RandomIntervalSpectralForest.html
+# - boss
+# https://sktime.org/modules/auto_generated/sktime.classification.dictionary_based.BOSSEnsemble.html
+# - st
+# https://sktime.org/modules/auto_generated/sktime.classification.shapelet_based.ShapeletTransformClassifier.html
 
 # used only for the sktime algorithms
 def from_table_to_data(x):
@@ -55,12 +67,7 @@ def from_table_to_data(x):
     
     return x_data
 
-# number of runs
-#numtotal = 30
-#numtotal = 1
-#startseq = 1
-
-if (len(sys.argv) != 5):
+if (len(sys.argv) != 6):
     quit('Error on arguments')
 
 # RAW time series
@@ -70,12 +77,17 @@ d_path = 'data/asos/1min'
 d_name = 'asos_2020_jan_1day_1min' 
 seed=1
 impute_method='linear'
+algorithm='randf'
 
 # getting arguments
 d_path=sys.argv[1]
 d_name=sys.argv[2]
 seed=int(sys.argv[3])
 impute_method=sys.argv[4]
+algorithm=sys.argv[5]
+
+# percent to split train/test
+train_pct=0.8
 
 # loading data
 X = pd.read_csv(d_path+'/'+d_name+'.csv', header=None)
@@ -86,61 +98,44 @@ y = X.iloc[:,-1] # last column
 X = X.loc[:, X.columns != 0] # removing names
 X = X.iloc[:,:-1] # all columns except last
 
-print(d_name, end=' ', flush=True)
+##########################
+## SPLITING TRAIN/TEST
+##########################
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+        train_size=train_pct, random_state=seed)
+X_train = pd.DataFrame(X_train)
+X_test = pd.DataFrame(X_test)
 
 
 ##########################
 ## PRE-PROCESSING
 ##########################
 
-# performing imputation
+# performing imputation (filling NA values)
+# train
 t = time.process_time()
-# mean
-#X = pd.DataFrame(impy.mean(X.values.transpose()).transpose(),
-#                        index=X.index, columns=X.columns)
-# locf
-#X = pd.DataFrame(impy.locf(X.values).transpose(),
-#                        index=X.index, columns=X.columns)
-# moving window
-#X = pd.DataFrame(impy.moving_window(X.values),
-#                        index=X.index, columns=X.columns)
-# filling NA values
-X.interpolate(method=impute_method, axis=1, inplace=True,limit_direction='both')
-#X.interpolate(method='linear', axis=1, inplace=True,limit_direction='both')
-#X.interpolate(method='polynomial', order=3,axis=1,inplace=True,limit_direction='both')
-#X.interpolate(method='spline', order=3, axis=1, inplace=True,limit_direction='both')
-#X.interpolate(method='cubicspline', axis=1, inplace=True,limit_direction='both')
-#X.fillna(0, inplace=True) # for filling the sides
-lap = time.process_time() - t
-print('impute: '+str(lap), end=' ', flush=True)
-
-
+X_train.interpolate(method=impute_method, axis=1, inplace=True, limit_direction='both')
+t_impute_train = time.process_time() - t
+# test
+t = time.process_time()
+X_test.interpolate(method=impute_method, axis=1, inplace=True, limit_direction='both')
+t_impute_test = time.process_time() - t
 
 # standardization of time series data
 # NOTE: since time series points can not be considered features, the
 # standardization must occur by rows, not columns
+# train
 t = time.process_time()
 scaler = StandardScaler()
-X = pd.DataFrame(scaler.fit_transform(X.values.transpose()).transpose(),
-                        index=X.index, columns=X.columns)
-#X = pd.DataFrame(scaler.fit_transform(X.values),
-#                        index=X.index, columns=X.columns)
-lap = time.process_time() - t
-print('scale: '+str(lap), end=' ', flush=True)
-
-#print(X)
-#print(X.shape)
-
-# spliting train/test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
-
-
-# standardization of time series data
-#scaler = StandardScaler()
-#X_train = pd.DataFrame(scaler.fit_transform(X_train.values.transpose()).transpose(),
-#                        index=X_train.index, columns=X_train.columns)
-#X_test = pd.DataFrame(scaler.fit_transform(X_test.values.transpose()).transpose(),
-#                        index=X_test.index, columns=X_test.columns)
+X_train = pd.DataFrame(scaler.fit_transform(X_train.values.transpose()).transpose(), 
+        index=X_train.index, columns=X_train.columns)
+t_scale_train = time.process_time() - t
+# test
+t = time.process_time()
+scaler = StandardScaler()
+X_test = pd.DataFrame(scaler.fit_transform(X_test.values.transpose()).transpose(), 
+        index=X_test.index, columns=X_test.columns)
+t_scale_test = time.process_time() - t
 
 # converting to sktime format
 X_train2 = from_table_to_data(X_train)
@@ -148,63 +143,77 @@ X_test2 = from_table_to_data(X_test)
 y_train2 = np.asarray(y_train)
 y_test2 = np.asarray(y_test)
 
+
+##########################
+## TRAINING
+##########################
+
+t = time.process_time()
+
 # classifiers
+if algorithm == 'knn':
+    clf = KNeighborsTimeSeriesClassifier(n_neighbors=1, metric="dtw")
+elif algorithm == 'randf':
+    clf = RandomForestClassifier(n_estimators=200, 
+                                 criterion='entropy',
+                                 min_samples_split=2,
+                                 random_state=seed)
+elif algorithm == 'tsf':
+    clf = TimeSeriesForestClassifier(n_estimators=200, 
+                                     criterion='entropy', 
+                                     min_samples_split=2,
+                                     random_state=seed)
+elif algorithm == 'rise':
+    clf = RandomIntervalSpectralForest(n_estimators=200,
+                                       #min_interval=16, 
+                                       #acf_lag=100, 
+                                       #acf_min_values=4,
+                                       random_state=seed)
+elif algorithm == 'boss':
+    clf = BOSSEnsemble()
+elif algorithm == 'st':
+    clf = ShapeletTransformClassifier()
 
-# knn
+# randf is the unique from sklearn
+if algorithm == 'randf':
+    clf.fit(X_train, y_train)
+else:
+    clf.fit(X_train2, y_train2)
+
+t_train = time.process_time() - t
+
+##########################
+## TESTING
+##########################
+
 t = time.process_time()
-knn = KNeighborsTimeSeriesClassifier(metric="dtw")
-knn.fit(X_train2, y_train2) # ignore the FutureWarning from sklearn
-y_pred = knn.predict(X_test2)
+
+if algorithm == 'randf':
+    y_pred = clf.predict(X_test)
+else:
+    y_pred = clf.predict(X_test2)
+
+t_test = time.process_time() - t
+
+# computing accuracy
 acc = accuracy_score(y_test, y_pred)
-lap = time.process_time() - t
-print('knn: '+str(acc)+' '+str(lap), end=' ', flush=True)
 
-# randf
-t = time.process_time()
-randf = RandomForestClassifier(random_state=seed)
-randf.fit(X_train, y_train)
-y_pred = randf.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
-lap = time.process_time() - t
-print('randf: '+str(acc)+' '+str(lap), end=' ', flush=True)
+# printing string y_test-y_pred
+print(d_name+' '+algorithm+' PREDICTED ' + \
+      ','.join(str(a)+'-'+str(b) for a,b in zip(y_test,y_pred)), 
+      flush=True)
 
-# tsf
-t = time.process_time()
-tsf = TimeSeriesForestClassifier(random_state=seed)
-tsf.fit(X_train2, y_train2)
-y_pred = tsf.predict(X_test2)
-acc = accuracy_score(y_test, y_pred)
-lap = time.process_time() - t
-print('tsf: '+str(acc)+' '+str(lap), end=' ', flush=True)
+print(d_name+' '+algorithm+' TIME_TRAIN' + \
+     ' impute: '+str(t_impute_train) + \
+     ' scale: '+str(t_scale_train) + \
+     ' train: '+str(t_train), flush=True)
 
-# rise
-t = time.process_time()
-rise = RandomIntervalSpectralForest(random_state=seed)
-rise.fit(X_train2, y_train2)
-y_pred = rise.predict(X_test2)
-acc = accuracy_score(y_test, y_pred)
-lap = time.process_time() - t
-print('rise: '+str(acc)+' '+str(lap), end=' ', flush=True)
+print(d_name+' '+algorithm+' TIME_TEST' + \
+     ' impute: '+str(t_impute_test) + \
+     ' scale: '+str(t_scale_test) + \
+     ' test: '+str(t_test), flush=True)
 
-#t = time.process_time()
-#st = ShapeletTransformClassifier()
-#st.fit(X_train2, y_train2)
-#acc = st.score(X_test2, y_test2)
-#lap = time.process_time() - t
-#print('st: '+str(acc)+' '+str(lap), end=' ', flush=True)
+print(d_name+' '+algorithm, end=' ', flush=True)
+print('FINAL_ACC '+str(acc), flush=True)
 
-# TODO: check if remove boss for less i's
-#if i >= 15:
-##if i >= 0:
-#    print('boss: 0 0', flush=True)
-#else:
-#    t = time.process_time()
-#    boss = BOSSEnsemble()
-#    boss.fit(X_train2, y_train2)
-#    y_pred = boss.predict(X_test2)
-#    acc = accuracy_score(y_test, y_pred)
-#    lap = time.process_time() - t
-#    print('boss: '+str(acc)+' '+str(lap), flush=True)
-
-print(' ', flush=True)
 
--- classify_sktime_asos_1min.sh
+++ classify_sktime_asos_1min.sh
@@ -1,8 +1,8 @@
 
 # script for automatizing the classification call
 
-#total=30 # also for the seed
-total=10 # also for the seed
+total=30 # also for the seed
+#total=10 # also for the seed
 seq_begin=1
 
 dataset_path='./data/asos/1min'
@@ -16,17 +16,19 @@ time_span=$1
 # spline/linear/polynomial
 method=$2
 
+# knn,randf,tsf,rise,boss,st
+algorithm=$3
+
 for time_int in "${time_int_l[@]}"
 do 
     for seed in $(seq $seq_begin $((seq_begin + total - 1)))
     do
-        #d_name="asos_2020_jan_"$time_span"_"$time_int"_"$method
         d_name="asos_2020_jan_"$time_span"_"$time_int
         
         #echo $d_name $seed
         #continue
 
-        python3 classify_sktime.py $dataset_path $d_name $seed $method
+        python3 classify_sktime.py $dataset_path $d_name $seed $method $algorithm
         
     done
 done
