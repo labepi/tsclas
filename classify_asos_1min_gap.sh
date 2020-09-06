cfe370e6435bb46361e7ea579f32bed59c61bcae
Joao Borges <joaoborges@dct.ufrn.br>
2020-09-05 23:25:56 -0300

    adding scripts for classifying 1min gaps

--- classify_asos_1min_gap.sh
+++ classify_asos_1min_gap.sh
@@ -1,8 +1,10 @@
 
 # script for automatizing the classification call
 
+# the mebedding dimension
+D=$1
+
 total=30 # also for the seed
-#total=10 # also for the seed
 seq_begin=1
 
 dataset_path='./data/asos/1min/gap'
@@ -11,26 +13,20 @@ dataset_path='./data/asos/1min/gap'
 time_int_l=( "1min" "5min" "10min" "15min" )
 
 # NOTE: making timespan as an argument, to run separated processes
-time_span=$1
+time_span=$2
 
-# spline/linear/polynomial
-method=$2
-
-# knn,randf,tsf,rise,boss,st
-algorithm=$3
-
-gap_num=$4
+gap_num=$3
 
 for time_int in "${time_int_l[@]}"
 do 
-    for seed in $(seq $seq_begin $((seq_begin + total - 1)))
+    for seed in $(seq $seq_begin $((seq_begin+total-1)))
     do
         d_name="asos_2020_jan_"$time_span"_"$time_int"_gap"$gap_num
         
         #echo $d_name $seed
         #continue
 
-        python3 classify_sktime.py $dataset_path $d_name $seed $method $algorithm
+        Rscript classify.R $dataset_path $d_name $D $seed 
         
     done
 done
