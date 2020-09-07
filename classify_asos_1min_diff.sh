97e66ef36f9e628f4377a2ce5433c666786f4ebc
Joao Borges <joaoborges@dct.ufrn.br>
2020-09-07 01:55:48 -0300

    adding scripts for classification 1min diff

--- classify_asos_1min_diff.sh
+++ classify_asos_1min_diff.sh
@@ -1,11 +1,17 @@
 
 # script for automatizing the classification call
 
-# the mebedding dimension
+if [ $# -ne 3 ]
+then
+    echo "Error: $0 D span1 span2"
+    exit
+fi
+
+# embedding dimension
 D=$1
 
-total=30 # also for the seed
 seq_begin=1
+total=30 # also for the seed
 
 dataset_path='./data/asos/1min'
 
@@ -13,18 +19,21 @@ dataset_path='./data/asos/1min'
 time_int_l=( "1min" "5min" "10min" "15min" )
 
 # NOTE: making timespan as an argument, to run separated processes
-time_span=$2
+time_span1=$2
+time_span2=$3
 
 for time_int in "${time_int_l[@]}"
 do 
     for seed in $(seq $seq_begin $((seq_begin+total-1)))
     do
-        d_name="asos_2020_jan_"$time_span"_"$time_int
+        d_name_train="asos_2020_jan_"$time_span1"_"$time_int
+        d_name_test="asos_2020_jan_"$time_span2"_"$time_int
         
         #echo $d_name $seed
+
         #continue
 
-        Rscript classify.R $dataset_path $d_name $D $seed 
+        Rscript classify_diff.R $dataset_path $d_name_train $d_name_test $D $seed
         
     done
 done
