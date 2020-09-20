34f8519d416a3740b8d9753fa56c2d65f9a7784d
Joao Borges <joaoborges@dct.ufrn.br>
2020-09-19 22:39:02 -0300

    adding script to collect 6 months in 1hour

--- data/asos/1hour/crawler_iem_1hour.sh
+++ data/asos/1hour/crawler_iem_1hour.sh
@@ -9,7 +9,8 @@ stations_file="airports"$1".csv"
 save_dir='./raw'
 
 url1="https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?station="
-url2="&data=all&year1=2020&month1=1&day1=1&year2=2020&month2=3&day2=31&tz=Etc%2FUTC&format=onlycomma&latlon=yes&missing=M&trace=T&direct=no&report_type=1&report_type=2"
+#url2="&data=all&year1=2020&month1=1&day1=1&year2=2020&month2=3&day2=31&tz=Etc%2FUTC&format=onlycomma&latlon=yes&missing=M&trace=T&direct=no&report_type=1&report_type=2"
+url2="&data=all&year1=2020&month1=1&day1=1&year2=2020&month2=6&day2=30&tz=Etc%2FUTC&format=onlycomma&latlon=yes&missing=M&trace=T&direct=no&report_type=1&report_type=2"
 
 for i in $(cat $stations_file | cut -d',' -f2 | grep -v ident | sed 's/"//g')
 do
